50c16143ff1e07d5592eaaa4aec1f7ed1b46446b
reizamuzayy <55563860+reizamuzayy@users.noreply.github.com>
2023-09-02 21:58:38 +0800

    Delete app/src/main/cpp directory

--- app/src/main/cpp/bre2/breStartup.c
+++
@@ -1,8 +0,0 @@
-#include <AEE_OEM.h>
-
-void StartLauncherApp( void *pUnused ) {
-    IShell *pShell = AEE_GetShell();
-    if(NULL != pShell) {
-        ISHELL_StartApplet(pShell, AEECLSID_APPMANAGER);
-    }
-}
